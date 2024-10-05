d35da08d2a111b3e1417b8164fa6857fadfb5176
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-05 21:41:28 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,4 +5,4 @@ import requests , json , random
 app = Flask(__name__)
 @app.route('/')
 def main():
-    return '此站点属于FantasyTeam---由vercel severless提供部署服务【正在建设中】'
+    return '此站点属于FantasyTeam---由vercel severless提供部署服正在建设中】'
