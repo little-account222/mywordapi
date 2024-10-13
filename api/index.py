3852b43de1e33371d1d1b72fdc7856f5180855c1
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-13 08:32:22 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+a = 1
 app=Flask(__name__)
 @app.route("/")
 def main():
+    a+=1
     return "ok"
+@app.route("/z")
+def add():
+    return a
