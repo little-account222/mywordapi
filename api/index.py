a974a4c3c328f6e9d903830c4da55393ec96d7c1
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-13 16:12:11 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,7 @@
+
 from flask import Flask
-import threading
-import time
+from login import login_creater
 
 app = Flask(__name__)
-
-# 定义全局变量
-a = 0
-
-def increment_a():
-    global a
-    while True:
-        a += 1
-        time.sleep(1)  # 等待1秒钟
-
-@app.route("/")
-def main():
-    return f"Counter: {a}666"
-thread = threading.Thread(target=increment_a)
-thread.start()
-
+app.register_blueprint(login_creater, url_prefix='/login')
+app.json.ensure_ascii = False
