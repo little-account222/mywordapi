a5909f8e9c3a0f5109e210a1147d41efa1475267
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-13 16:24:31 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
-from login import login_creater
+# from login import login_creater
 
-app = Flask(__name__)
-app.register_blueprint(login_creater, url_prefix='/login')
-app.json.ensure_ascii = False
+# app = Flask(__name__)
+# app.register_blueprint(login_creater, url_prefix='/login')
+# app.json.ensure_ascii = False
