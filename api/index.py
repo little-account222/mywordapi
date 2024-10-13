0eac55479d78b9c47912fcd6b2f8e85ea9eca4a7
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-13 08:50:05 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,19 +7,15 @@ app = Flask(__name__)
 # 定义全局变量
 a = 0
 
-# 创建锁对象以确保对共享资源的安全访问
-lock = threading.Lock()
-
 def increment_a():
     global a
     while True:
-        with lock:  # 使用锁来保护对全局变量的修改
-            a += 1
+        a += 1
         time.sleep(1)  # 等待1秒钟
 
 @app.route("/")
 def main():
-    return f"Counter: {a}"
+    return f"Counter: {a}666"
 
 @app.route("/z")
 def add():
