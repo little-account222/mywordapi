d2633a60c8628201b25477427010e5ddfb0eec2b
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-19 16:07:12 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,39 +1,15 @@
-
+import sys
+import os
 from flask import Flask
 from .login import login_creater
-# 在 ./api/index.py 文件中 
-
-import sys  
-import os  
-
-# 获取当前文件的绝对路径  
-current_file_path = os.path.abspath(__file__)  
- 
-# 获取当前文件所在目录的路径 
-current_dir_path = os.path.dirname(current_file_path)
-
-# 计算到 Main.py 文件的相对路径
-main_module_path = os.path.join(current_dir_path, '..', 'Kitten-4-Decompiler-main', 'Kitten-4-Decompiler-main')
-
-# 确保该路径不在 sys.path 中
-if main_module_path not in sys.path:
-    sys.path.append(main_module_path)
-
-# 现在可以导入 Main 模块
-import Main
-
-# 你现在可以使用 Main 模块中的任何函数或类 
-# 例如：Main.some_function()
-
+from .kittenserver import kitten_creater
 
 app = Flask(__name__)
+app.register_blueprint(login_creater, url_prefix='/login')
+app.register_blueprint(kitten_creater, url_prefix='/kitten')
+app.json.ensure_ascii = False
+
 @app.route("/")
 def main():
-  return Main.main(240362475)
+    return '服务器正常运行中'
 
-
-@app.route("/z")
-def zrrr():
-  return "6666"
-app.register_blueprint(login_creater, url_prefix='/login')
-app.json.ensure_ascii = False
