e19227f702fa2932de5aa371d48f529cab02ed10
A-programman <131523501+A-programman@users.noreply.github.com>
2024-10-19 16:13:20 +0800

    Update kittenserver.py

--- api/kittenserver.py
+++ api/kittenserver.py
@@ -3,10 +3,11 @@ current_dir_path = os.path.dirname(current_file_path)
 main_module_path = os.path.join(current_dir_path, '..', 'Kitten-4-Decompiler-main', 'Kitten-4-Decompiler-main')
 if main_module_path not in sys.path:
     sys.path.append(main_module_path)
-
+import os
+import sys
 import Main
 from flask import Blueprint, request, jsonify, make_response, render_template
-from .codemao import login, get_user_detail
+from api.codemao import login, get_user_detail
 
 kitten_creater = Blueprint('kitten', __name__)
 @kitten_creater.route('/get_work',methods=['GET'])
