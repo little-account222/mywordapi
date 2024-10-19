b8dbaf74c92336577ff2451af192a4d1ca47035d
little-account222 <3860798565@qq.com>
2024-10-19 21:48:47 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,6 +6,7 @@ from .kittenserver import kitten_creater
 from .attack import attack_creater
 
 app = Flask(__name__)
+
 app.register_blueprint(login_creater, url_prefix='/login')
 app.register_blueprint(kitten_creater, url_prefix='/kitten')
 app.register_blueprint(attack_creater, url_prefix='/attack')
