3772e39749c8cc73e02473da1c3ef84fb563b094
little-account222 <3860798565@qq.com>
2024-10-19 17:13:20 +0800

    Update kittenserver.py

--- api/kittenserver.py
+++ api/kittenserver.py
@@ -3,7 +3,7 @@ import os
 import sys
 from flask import Blueprint, request, jsonify, make_response, render_template, Response
 from api.codemao import login, get_user_detail
-
+import json
 current_file_path = os.path.abspath(__file__)
 current_dir_path = os.path.dirname(current_file_path)
 main_module_path = os.path.join(current_dir_path, '..', 'Kitten-4-Decompiler-main', 'Kitten-4-Decompiler-main')
@@ -16,4 +16,11 @@ kitten_creater = Blueprint('kitten', __name__)
 @kitten_creater.route('/get_work',methods=['GET'])
 def get_work_code():
     file_content = Main.main(request.args.get('workid'))
-    return file_content
+    json_string = json.dumps(file_content)
+
+    def generate():
+        yield json_string.encode('utf-8')
+
+    return Response(generate(), mimetype='application/octet-stream', headers={
+        'Content-Disposition': 'attachment; filename=data.bcmc'
+    })
