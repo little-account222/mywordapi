5661c076b9f46d05c674b8a2d131ddf0e62f9f38
little-account222 <3860798565@qq.com>
2024-10-19 16:41:30 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
 import sys
 import os
 from flask import Flask
-# from api.login import login_creater
-# from api.kittenserver import kitten_creater
+from .login import login_creater
+from .kittenserver import kitten_creater
 
 app = Flask(__name__)
-# app.register_blueprint(login_creater, url_prefix='/login')
-# app.register_blueprint(kitten_creater, url_prefix='/kitten')
-# app.json.ensure_ascii = False
+app.register_blueprint(login_creater, url_prefix='/login')
+app.register_blueprint(kitten_creater, url_prefix='/kitten')
+app.json.ensure_ascii = False
 
 @app.route("/")
 def main(): 
