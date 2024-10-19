8390567c9d097a4f3c52ba9ccef32be01e1eb927
little-account222 <3860798565@qq.com>
2024-10-19 21:47:32 +0800

    Update attack.py

--- api/attack.py
+++ api/attack.py
@@ -4,7 +4,7 @@ from .codemao import login, get_user_detail, comfirm_account, post_comment
 attack_creater = Blueprint('attack', __name__)
 
 
-@attack_creater.route('/dom/post/<int:pid>',methods=['GET'])
+@attack_creater.route('/dom/post/more/<int:pid>',methods=['GET'])
 def attack_dom_post(pid):
     active = comfirm_account(request.cookies.get('token'))
     if active == 'succ':
