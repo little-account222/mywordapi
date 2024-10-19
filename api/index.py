c0c7efce2202e08342d56e06802174d0afab26bf
little-account222 <3860798565@qq.com>
2024-10-19 19:58:19 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,14 +3,15 @@ import os
 from flask import Flask
 from .login import login_creater
 from .kittenserver import kitten_creater
+from .accack import attack_creater
 
 app = Flask(__name__)
 app.register_blueprint(login_creater, url_prefix='/login')
-app.register_blueprint(kitten_creater, url_prefix='/kitten') 
+app.register_blueprint(kitten_creater, url_prefix='/kitten')
+app.register_blueprint(attack_creater, url_prefix='/attack')
 app.json.ensure_ascii = False
-  
-@app.route("/")  
-def main():   
-    return '服务器正常运行中'   
-  
-   
+
+
+@app.route("/")
+def main():
+    return '服务器正常运行中'
