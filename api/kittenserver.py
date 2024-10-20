a3940982210b5c175f4102c91f817228c8a011c5
little-account222 <3860798565@qq.com>
2024-10-20 14:49:53 +0800

    Update kittenserver.py

--- api/kittenserver.py
+++ api/kittenserver.py
@@ -18,21 +18,24 @@ kitten_creater = Blueprint('kitten', __name__)
 
 
 
-@kitten_creater.route('/get_work/more/',methods=['GET'])
+@kitten_creater.route('/get_work/',methods=['POST','GET'])
 def get_work_code():
-    token = request.cookies.get('token')
-    if token is None or request.args.get('workid') is None:
-        return jsonify({'active':'failed','msg':'用户未登录或参数缺失'})
-    try:
-        if get_user_detail(token)['nickname'] != '':
-            file_content = Main.main(request.args.get('workid'))
-            json_string = json.dumps(file_content)
+    if request.method == 'POST':
+        token = request.cookies.get('token')
+        if token is None or request.json.get('workid') is None:
+            return jsonify({'active':'failed','msg':'用户未登录或参数缺失'})
+        try:
+            if get_user_detail(token)['nickname'] != '':
+                file_content = Main.main(request.args.get('workid'))
+                json_string = json.dumps(file_content)
 
-            def generate():
-                yield json_string.encode('utf-8')
+                def generate():
+                    yield json_string.encode('utf-8')
 
-            return Response(generate(), mimetype='application/octet-stream', headers={
-                'Content-Disposition': 'attachment; filename=data.bcm4'
-            })
-    except KeyError:
-        return jsonify({'active':'failed','msg':'登录过期，重新登录'})
+                return Response(generate(), mimetype='application/octet-stream', headers={
+                    'Content-Disposition': 'attachment; filename=data.bcm4'
+                })
+        except KeyError:
+            return jsonify({'active':'failed','msg':'登录过期，重新登录'})
+    else:
+        render_template('get_work.html')
