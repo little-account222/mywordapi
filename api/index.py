58f255e79c0268500ddd982218d3bd5ad837d174
little-account222 <3860798565@qq.com>
2024-10-25 20:09:39 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.register_blueprint(login_creater, url_prefix='/login')
 app.register_blueprint(kitten_creater, url_prefix='/kitten')
-app.register_blueprint(attack_creater, url_prefix='/attack') 
+app.register_blueprint(attack_creater, url_prefix='/attack')  
 app.json.ensure_ascii = False 
   
   
