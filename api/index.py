45158246dad26b508332c49d26b538deb097f9b6
little-account222 <3860798565@qq.com>
2024-10-26 14:30:44 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ app.register_blueprint(login_creater, url_prefix='/login')
 app.register_blueprint(kitten_creater, url_prefix='/kitten')
 app.register_blueprint(attack_creater, url_prefix='/attack')  
 app.json.ensure_ascii = False 
-  
+   
   
 @app.route("/") 
 def main():  
