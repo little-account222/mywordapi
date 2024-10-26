47f448da99a09b9e7c03d50fec74c7df3da791c3
little-account222 <3860798565@qq.com>
2024-10-26 14:59:05 +0800

    Update attack.py

--- api/attack.py
+++ api/attack.py
@@ -1,32 +1,29 @@
 from flask import Blueprint, request, jsonify, make_response, render_template
-from flask_cors import cross_origin  # 只需要导入cross_origin
 from .codemao import login, get_user_detail, comfirm_account, post_comment, invite_user
+from flask_cors import cross_origin  # 只需要导入cross_origin
+attack_creater = Blueprint('attack', __name__)
 
-attack_creator = Blueprint('attack', __name__)
 
-@attack_creator.route('/dom/post/more/<int:pid>', methods=['GET'])
+@attack_creater.route('/dom/post/more/<int:pid>',methods=['GET'])
 def attack_dom_post(pid):
-    active = confirm_account(request.cookies.get('token'))
+    active = comfirm_account(request.cookies.get('token'))
     if active == 'succ':
-        post_comment(pid, request.cookies.get('token'))
-        return jsonify({'active': 'successful'})
+        post_comment(pid,request.cookies.get('token'))
+        return jsonify({'active':'successful'})
     else:
-        return jsonify({'active': 'failed', 'msg': active})
+      return jsonify({'active':'failed','msg':active})
 
-info_list = {'clsid': 0, 'token': ''}
-
-@attack_creator.route('/class/info')
+info_list = {'clsid':0,'token':''}
+@attack_creater.route('/class/info')
 def get_info():
     return jsonify(info_list)
 
-@attack_creator.route('/class/info/edit/')
+@attack_creater.route('/class/info/edit/')
 def edit_info():
-    info_list['clsid'] = int(request.args.get('clsid'))
-    info_list['token'] = str(request.args.get('token'))
+    info_list = {'clsid':int(request.args.get('clsid')),'token':str(request.args.get('token'))}
     return jsonify(info_list)
-
-@attack_creator.route('/invite')
-@cross_origin(origins='https://coco.codemao.cn/')  # 在路由级别启用跨域支持
+@cross_origin(origins='https://coco.codemao.cn')
+@attack_creater.route('/invite')
 def invite():
     invite_user(request.args.get('username'))
     return 'ok'
